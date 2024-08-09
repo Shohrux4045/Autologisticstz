0ffcbf0a8f475fadbcf641ba2809e2c796676223
Shohrux <zokirovshohrux2020@gmail.com>
2024-08-09 18:51:56 +0500

    Сделал адимнку

--- news/admin.py
+++ news/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Category, News
 
-# Register your models here.
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name')
+    search_fields = ('name',)
+    ordering = ('name',)
+
+@admin.register(News)
+class NewsAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'date', 'category')
+    search_fields = ('title', 'text')
+    list_filter = ('category', 'date')
+    ordering = ('-date',)
